d471d34d549eafa10a4d838200a740cf4d660ffb
Tomás <compaqle@gmail.com>
2026-04-25 22:08:26 +0100

    Implementação inicial da classe Produto (Header)

---
+++ Project/include/model/Produto.h
@@ -0,0 +1,26 @@
+#include <string>
+
+class Produto {
+    private:
+    int id;
+    std::string nome;
+    double preco_base;
+    int stock;
+    int id_categoria;
+
+    public:
+    Produto(int id, std::string nome, double preco_base, int stock, int id_categoria);
+
+    int getId();
+    std::string getNome();
+    double getPrecoBase();
+    int getStock();
+    int getIdCategoria();
+    
+    void setNome(std::string novoNome);
+    void setPrecoBase(double novoPreco);
+    
+    void addStock(int quantidade);
+    void removerStock(int quantidade);
+
+};
