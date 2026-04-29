8ec90976419441f7305d26bc101fb56fe1815ce9
Tomás <compaqle@gmail.com>
2026-04-29 12:16:16 +0100

    update

--- Project/src/model/Produto.cpp
+++ Project/src/model/Produto.cpp
@@ -2,7 +2,6 @@
 
 namespace model {
 
-// CONSTRUTOR
 Produto::Produto(int id, std::string nome, double preco_base, int stock, int id_categoria) {
     this->id = id;
     this->nome = nome;
@@ -11,18 +10,15 @@ Produto::Produto(int id, std::string nome, double preco_base, int stock, int id_
     this->id_categoria = id_categoria;
 }
 
-// GETTERS
 int Produto::getId() { return id; }
 std::string Produto::getNome() { return nome; }
 double Produto::getPrecoBase() { return preco_base; }
 int Produto::getStock() { return stock; }
 int Produto::getIdCategoria() { return id_categoria; }
 
-// SETTERS
 void Produto::setNome(std::string novoNome) { this->nome = novoNome; }
 void Produto::setPrecoBase(double novoPreco) { this->preco_base = novoPreco; }
 
-// GESTÃO DE STOCK
 void Produto::addStock(int quantidade) {
     this->stock += quantidade;
 }
