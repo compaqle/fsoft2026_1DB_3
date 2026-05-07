2458983e72bf3ea58747781294bc4de4cd3ddbb8
Tomás <compaqle@gmail.com>
2026-05-07 18:37:17 +0100

    feat: implemented AdminView and CaixaView menu loops

--- Project/src/view/AdminView.cpp
+++ Project/src/view/AdminView.cpp
@@ -4,9 +4,26 @@
 AdminView::AdminView() {}
 
 void AdminView::exibirMenuAdmin() {
-    std::cout << "\n--- MENU ADMINISTRADOR ---" << std::endl;
-    std::cout << "1. Gerir Catalogo" << std::endl;
-    std::cout << "2. Gerir Clientes" << std::endl;
-    std::cout << "3. Ver Estatisticas" << std::endl;
-    std::cout << "0. Sair" << std::endl;
+    int opcao;
+    while (true) {
+        std::cout << "\n--- MENU ADMIN ---" << std::endl;
+        std::cout << "1. Gerir Catalogo" << std::endl;
+        std::cout << "2. Gerir Clientes" << std::endl;
+        std::cout << "3. Ver Estatisticas" << std::endl;
+        std::cout << "0. Sair" << std::endl;
+        std::cout << "\nOpcao: ";
+        std::cin >> opcao;
+
+        if (opcao == 0) {
+            return;
+        } else if (opcao == 1) {
+            std::cout << "Gerir Catalogo - em desenvolvimento" << std::endl;
+        } else if (opcao == 2) {
+            std::cout << "Gerir Clientes - em desenvolvimento" << std::endl;
+        } else if (opcao == 3) {
+            std::cout << "Ver Estatisticas - em desenvolvimento" << std::endl;
+        } else {
+            std::cout << "Opcao invalida. Tente novamente." << std::endl;
+        }
+    }
 }
--- Project/src/view/CaixaView.cpp
+++ Project/src/view/CaixaView.cpp
@@ -4,9 +4,26 @@
 CaixaView::CaixaView() {}
 
 void CaixaView::exibirMenuCaixa() {
-    std::cout << "\n--- MENU CAIXA ---" << std::endl;
-    std::cout << "1. Realizar Venda" << std::endl;
-    std::cout << "2. Consultar Preco" << std::endl;
-    std::cout << "3. Consultar Pontos Cliente" << std::endl;
-    std::cout << "0. Sair" << std::endl;
+    int opcao;
+    while (true) {
+        std::cout << "\n--- MENU CAIXA ---" << std::endl;
+        std::cout << "1. Realizar Venda" << std::endl;
+        std::cout << "2. Consultar Preco" << std::endl;
+        std::cout << "3. Consultar Pontos Cliente" << std::endl;
+        std::cout << "0. Sair" << std::endl;
+        std::cout << "\nOpcao: ";
+        std::cin >> opcao;
+
+        if (opcao == 0) {
+            return;
+        } else if (opcao == 1) {
+            std::cout << "Realizar Venda - em desenvolvimento" << std::endl;
+        } else if (opcao == 2) {
+            std::cout << "Consultar Preco - em desenvolvimento" << std::endl;
+        } else if (opcao == 3) {
+            std::cout << "Consultar Pontos Cliente - em desenvolvimento" << std::endl;
+        } else {
+            std::cout << "Opcao invalida. Tente novamente." << std::endl;
+        }
+    }
 }
