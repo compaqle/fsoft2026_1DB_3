317c5fb0b6eb31f1a7349e30a6ee47e00e1ecc40
Tomás <compaqle@gmail.com>
2026-05-26 16:05:58 +0100

    restaurar metodos do SupermercadoRepository com ponteiros (resolve conflitos e ficheiros apagados)

--- Project/include/repo/SupermercadoRepository.h
+++ Project/include/repo/SupermercadoRepository.h
@@ -3,6 +3,10 @@
 #include <string>
 #include "../model/Produto.h"
 #include "../model/Categoria.h"
+#include "../model/Cliente.h"
+#include "../model/Caixa.h"
+#include "../model/Venda.h"
+#include "../model/Promocao.h"
 
 class SupermercadoRepository {
 private:
@@ -10,12 +14,20 @@ private:
 
     std::vector<Produto*> produtos;
     std::vector<Categoria*> categorias;
+    std::vector<Cliente*> clientes;
+    std::vector<Caixa*> caixas;
+    std::vector<Venda*> vendas;
+    std::vector<Promocao*> promocoes;
 
     SupermercadoRepository();
     ~SupermercadoRepository();
 
     void carregarProdutos();
     void carregarCategorias();
+    void carregarClientes();
+    void carregarCaixas();
+    void carregarVendas();
+    void carregarPromocoes();
 
 public:
     static SupermercadoRepository& getInstance();
@@ -25,4 +37,16 @@ public:
 
     std::vector<Categoria*>& getCategorias();
     void guardarCategorias();
+    
+    std::vector<Cliente*>& getClientes();
+    void guardarClientes();
+
+    std::vector<Caixa*>& getCaixas();
+    void guardarCaixas();
+
+    std::vector<Venda*>& getVendas();
+    void guardarVendas();
+
+    std::vector<Promocao*>& getPromocoes();
+    void guardarPromocoes();
 };
