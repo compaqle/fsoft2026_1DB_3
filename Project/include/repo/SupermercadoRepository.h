248793a83d73aa4b6f23e0bc73b16a6852c0b5d5
Guilherme <1251002@isep.ipp.pt>
2026-05-26 11:49:30 +0100

    remover funçoes mal feitas

--- Project/include/repo/SupermercadoRepository.h
+++ Project/include/repo/SupermercadoRepository.h
@@ -3,41 +3,19 @@
 #include <string>
 #include "../model/Produto.h"
 #include "../model/Categoria.h"
-#include "../model/Cliente.h"
-#include "../model/Venda.h"
-#include "../model/Promocao.h"
-#include "../model/Caixa.h"
-#include "../model/Venda.h"
-#include "../model/Promocao.h"
 
 class SupermercadoRepository {
 private:
     static SupermercadoRepository* instance;
 
-
-    std::vector<Produto> produtos;
-    std::vector<Categoria> categorias;
-    std::vector<Cliente> cliente;
-    std::vector<Venda> vendas;
-    std::vector<Promocao> promocoes;
-
-    std::vector<Produto*> produtos_ptr;
-    std::vector<Categoria*> categorias_ptr;
-    std::vector<Cliente*> clientes;
-    std::vector<Caixa*> caixas;
-    std::vector<Venda*> vendas_ptr;
-    std::vector<Promocao*> promocoes_ptr;
+    std::vector<Produto*> produtos;
+    std::vector<Categoria*> categorias;
 
     SupermercadoRepository();
     ~SupermercadoRepository();
 
     void carregarProdutos();
     void carregarCategorias();
-    void carregarClientes();
-
-    void carregarCaixas();
-    void carregarVendas();
-    void carregarPromocoes();
 
 public:
     static SupermercadoRepository& getInstance();
@@ -47,25 +25,4 @@ public:
 
     std::vector<Categoria*>& getCategorias();
     void guardarCategorias();
-    
-    std::vector<Cliente>& getClientes();
-    void guardarClientes();
-
-    std::vector<Venda>& getVendas();
-    void guardarVendas();
-
-    std::vector<Promocao>& getPromocoes();
-    void guardarPromocoes();
-
-    std::vector<Cliente*>& getClientes_ptr();
-    void guardarClientes_ptr();
-
-    std::vector<Caixa*>& getCaixas();
-    void guardarCaixas();
-
-    std::vector<Venda*>& getVendas_ptr();
-    void guardarVendas_ptr();
-
-    std::vector<Promocao*>& getPromocoes_ptr();
-    void guardarPromocoes_ptr();
 };
